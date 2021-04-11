ff869cae1d6de0e113aca16dca8c5c7517d58b85
Delta Dextra <delta.dex7@gmail.com>
2021-04-11 17:21:24 +0800

    Handle non-existent file

--- src/scraper/datamgr.ts
+++ src/scraper/datamgr.ts
@@ -5,12 +5,20 @@ import { UpdatedData } from "../server/database.ts";
 export function readCharacters(): UpdatedData<Character[]> {
   // TODO: handle when the file doesn't exist
 
-  const charsData = Deno.readTextFileSync(FILE_CHARACTERS);
-  console.log(`${charsData.length} bytes read from ${FILE_CHARACTERS}.`);
+  try {
+    const charsData = Deno.readTextFileSync(FILE_CHARACTERS);
+    console.log(`${charsData.length} bytes read from ${FILE_CHARACTERS}.`);
 
-  const chars: UpdatedData<Character[]> = JSON.parse(charsData);
+    const chars: UpdatedData<Character[]> = JSON.parse(charsData);
 
-  return chars;
+    return chars;
+  } catch (e) {
+    console.log(`${FILE_CHARACTERS} does not exist. Returning a dummy data.`);
+    return {
+      lastUpdated: "1970-01-01T00:00:00.000Z",
+      data: [],
+    };
+  }
 }
 
 export function writeCharacters(chars: UpdatedData<Character[]>) {
