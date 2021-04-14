6b03e4b00284aa2b18c5d342b922ae4ca1cd3ed6
Delta Dextra <delta.dex7@gmail.com>
2021-04-14 17:24:57 +0800

    Fix extract script

--- extract.bash
+++ extract.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-
+set -e
 sum1=`sha256sum characters.json`
 deno run --allow-net --allow-write extract.ts
 sum2=`sha256sum characters.json`
-if [ $sum1 != $sum2 ]; then
+if [[ $sum1 != $sum2 ]]; then
   git add characters.json
   git commit -m "Characters updated $(date -u +%F)" 
   git push
