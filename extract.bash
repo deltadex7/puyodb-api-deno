a77b2ed1765bd2e884505d47b3d6f1323effbc36
Delta Dextra <delta.dex7@gmail.com>
2021-06-05 21:32:41 +0800

    Fix error when no data is fetched
    Also don't commit if data is exactly the same.

--- extract.bash
+++ extract.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 set -e
 sum1=`sha256sum characters.json`
-deno run --allow-net --allow-write extract.ts
+deno run --allow-net --allow-write --allow-read extract.ts
 sum2=`sha256sum characters.json`
 if [[ $sum1 != $sum2 ]]; then
   git add characters.json
--- extract.ts
+++ extract.ts
@@ -1,10 +1,25 @@
 import { getAllCharacters } from "./src/scraper/extractor.ts";
-import { writeCharacters } from "./src/scraper/datamgr.ts";
+import { writeCharacters, readCharacters } from "./src/scraper/datamgr.ts";
+import { equal } from "https://deno.land/x/equal/mod.ts";
+
+const data = await getAllCharacters();
+
+if (data.length === 0) {
+  console.error("ERROR: Character data is empty! Exiting with error.");
+  Deno.exit(1)
+}
+
+const old_data = readCharacters();
+
+if (equal(data, old_data.data)) {
+  console.error("INFO: Data is exactly the same. Exiting normally.");
+  Deno.exit(0);
+} 
 
 const r = {
   lastUpdated: new Date().toISOString(),
   data: await getAllCharacters(),
 };
-
+  
 writeCharacters(r);
 
