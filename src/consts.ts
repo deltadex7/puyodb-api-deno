8336e8bdf82b5c8c5a117411298ff7d78b932624
deltadex7 <delta.dex7@gmail.com>
2022-07-24 16:52:38 +0700

    Indicate files as relative path
    deno deploy did not read the file before

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 export const BASE_URL = "https://puyonexus.com";
 export const WIKI_PREFIX = "/wiki/";
-export const FILE_CHARACTERS = "characters.json";
-export const FILE_QUEST = "quest.json";
+export const FILE_CHARACTERS = "./characters.json";
+export const FILE_QUEST = "./quest.json";
